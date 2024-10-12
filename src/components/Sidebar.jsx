8bf711d497a705d2f3696857ad5e69d39e9979a4
SalmanFikri00 <fikriyoyoyo02@gmail.com>
2024-10-12 07:49:13 +0700

    feat(sidebar): add and implement component sidebar

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,10 @@
+import logo from "../assets/Logo-Square.svg"
+const Sidebar = () => {
+  return (
+    <aside className=" bg-list h-screen p-5">
+        <img src={logo} alt="logo" width={28} />
+    </aside>
+  )
+}
+
+export default Sidebar
