6d3ff731f713fe68266afa21f5b96b6b71438962
SalmanFikri00 <fikriyoyoyo02@gmail.com>
2024-10-12 07:45:08 +0700

    init: initialize color and font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,17 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        list: "#1A0931",
+        primaryText: "#2F3136",
+        secondaryText: "#5D6372",
+        offText: "#B7BDC9",
+      },
+      fontFamily: {
+        openSans: ["Open Sans", "sans-serif"]
+      }
+    },
   },
   plugins: [],
 }
